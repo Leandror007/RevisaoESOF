daa2a0fc755e86145d296049a32cac6952f58de5
leandror007 <lrramalho@gmail.com>
2016-11-03 16:36:29 -0200

    Nova Funcionalidade ...ok!

---
+++ SistemaLeilao_v2/src/v2/TesteDoAvaliador.java
@@ -0,0 +1,37 @@
+/*
+ * Código fonte reproduzido do livro 
+ * Testes Automatizados: um guia prático
+ * Maurício Aniche
+ * Casa do Código
+ * http://www.casadocodigo.com.br/products/livro-testes-de-software
+ */
+package v2;
+
+/**
+ *
+ * @author saulo da mata
+ */
+public class TesteDoAvaliador {
+    
+    public static void main(String[] args) {
+        Usuario joao = new Usuario("Joao");
+        Usuario jose = new Usuario("Jose");
+        Usuario maria = new Usuario("Maria");
+
+        Leilao leilao = new Leilao("Playstation 3 novo");
+
+        leilao.propoe(new Lance(maria,250.0));
+        leilao.propoe(new Lance(joao,300.0));
+        leilao.propoe(new Lance(jose,400.0));
+        
+
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        //imprime 400
+        System.out.println("Maior lance: " + leiloeiro.getMaiorLance());
+        
+        //imprime 400
+        System.out.println("Menor lance: " + leiloeiro.getMenorLance());
+    }
+}
